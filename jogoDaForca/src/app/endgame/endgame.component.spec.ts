e7c71ba699b21cdef236a24ce6b043b14cc93b0c
GuiTodys <guilhermetodys123@hotmail.com>
2021-11-17 16:17:19 -0300

    Create endgame.component.spec.ts

---
+++ jogoDaForca/src/app/endgame/endgame.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EndgameComponent } from './endgame.component';
+
+describe('EndgameComponent', () => {
+  let component: EndgameComponent;
+  let fixture: ComponentFixture<EndgameComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EndgameComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EndgameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
